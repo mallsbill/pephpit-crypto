fd95703027d491d22093a998cff5b1a5f48becd3
Guillaume Malleret <guillaume.malleret@flex-multimedia.com>
2018-04-20 15:01:55 +0200

    méthodes pour définir les constantes MCRYPT si elles ne sont pas disponibles (PHP 7.2)

--- lib/Mcrypt2Openssl.php
+++ lib/Mcrypt2Openssl.php
@@ -13,6 +13,7 @@ class Mcrypt2Openssl {
 	 * @see https://github.com/tom--/mcrypt2openssl/blob/master/mapping.md
 	 */
 	public static function get($cipher, $mode, $keylength) {
+		self::defineMcryptConstants();
 
 		if($cipher == MCRYPT_BLOWFISH || $cipher == MCRYPT_BLOWFISH_COMPAT) {
 			if($mode == MCRYPT_MODE_CBC)
@@ -156,6 +157,53 @@ class Mcrypt2Openssl {
 		else {
 			return 16;
 		}
+	}
+
+	public static function defineMcryptConstants() {
+		if(defined('MCRYPT_ENCRYPT') === true)
+			return;
+
+		define ('MCRYPT_ENCRYPT', 0);
+		define ('MCRYPT_DECRYPT', 1);
+		define ('MCRYPT_DEV_RANDOM', 0);
+		define ('MCRYPT_DEV_URANDOM', 1);
+		define ('MCRYPT_RAND', 2);
+		define ('MCRYPT_3DES', "tripledes");
+		define ('MCRYPT_ARCFOUR_IV', "arcfour-iv");
+		define ('MCRYPT_ARCFOUR', "arcfour");
+		define ('MCRYPT_BLOWFISH', "blowfish");
+		define ('MCRYPT_BLOWFISH_COMPAT', "blowfish-compat");
+		define ('MCRYPT_CAST_128', "cast-128");
+		define ('MCRYPT_CAST_256', "cast-256");
+		define ('MCRYPT_CRYPT', "crypt");
+		define ('MCRYPT_DES', "des");
+		define ('MCRYPT_ENIGNA', "crypt");
+		define ('MCRYPT_GOST', "gost");
+		define ('MCRYPT_LOKI97', "loki97");
+		define ('MCRYPT_PANAMA', "panama");
+		define ('MCRYPT_RC2', "rc2");
+		define ('MCRYPT_RIJNDAEL_128', "rijndael-128");
+		define ('MCRYPT_RIJNDAEL_192', "rijndael-192");
+		define ('MCRYPT_RIJNDAEL_256', "rijndael-256");
+		define ('MCRYPT_SAFER64', "safer-sk64");
+		define ('MCRYPT_SAFER128', "safer-sk128");
+		define ('MCRYPT_SAFERPLUS', "saferplus");
+		define ('MCRYPT_SERPENT', "serpent");
+		define ('MCRYPT_THREEWAY', "threeway");
+		define ('MCRYPT_TRIPLEDES', "tripledes");
+		define ('MCRYPT_TWOFISH', "twofish");
+		define ('MCRYPT_WAKE', "wake");
+		define ('MCRYPT_XTEA', "xtea");
+		define ('MCRYPT_IDEA', "idea");
+		define ('MCRYPT_MARS', "mars");
+		define ('MCRYPT_RC6', "rc6");
+		define ('MCRYPT_SKIPJACK', "skipjack");
+		define ('MCRYPT_MODE_CBC', "cbc");
+		define ('MCRYPT_MODE_CFB', "cfb");
+		define ('MCRYPT_MODE_ECB', "ecb");
+		define ('MCRYPT_MODE_NOFB', "nofb");
+		define ('MCRYPT_MODE_OFB', "ofb");
+		define ('MCRYPT_MODE_STREAM', "stream");
 	}
 
 }
